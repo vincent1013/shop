714f2c9180f2a54a907ef62b983389a04d957399
zhouchuhao <zhouchuhao@meizu.com>
2022-07-01 13:59:22 +0800

    test

--- src/main/java/com/vito16/shop/ShopApplication.java
+++ src/main/java/com/vito16/shop/ShopApplication.java
@@ -19,6 +19,7 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 public class ShopApplication  {
 
 	public static void main(String[] args) {
+		System.out.println("test");
 		SpringApplication.run(ShopApplication.class,args);
 	}
 
